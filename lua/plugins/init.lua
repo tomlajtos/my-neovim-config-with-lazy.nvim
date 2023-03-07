fabc36f1efba2d3548916a4ad7a9f5ca0cfd9109
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-03-07 15:32:09 +0100

    initial setup

---
+++ lua/plugins/init.lua
@@ -0,0 +1,16 @@
+local Plugins = {
+  {'tpope/vim-fugitive'},
+  {'wellle/targets.vim'},
+  {'tpope/vim-repeat'},
+  {'editorconfig/editorconfig-vim'},
+  {'kyazdani42/nvim-web-devicons', lazy = true},
+  {'numToStr/Comment.nvim', config = true, event = 'VeryLazy'},
+
+  -- Themes
+  {'folke/tokyonight.nvim'},
+  {'joshdick/onedark.vim'},
+  {'tanvirtin/monokai.nvim'},
+  {'lunarvim/darkplus.nvim'},
+}
+
+return Plugins
