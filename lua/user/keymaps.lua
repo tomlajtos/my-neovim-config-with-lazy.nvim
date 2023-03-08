f592aa676a940f02f0c6a47333c99a7f0f08ca9e
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-03-08 12:58:41 +0100

    add key bindings and settings

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,20 +1,81 @@
--- Space as leader key
+-- Set leader to "<space>"
 vim.g.mapleader = ' '
 
+-- Set keymap for nvim default file browser
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open Netrw explorer" })
+
 -- Shortcuts
-vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '^')
-vim.keymap.set({'n', 'x', 'o'}, '<leader>l', 'g_')
-vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>h', '^')
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>l', 'g_')
+-- vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
 
 -- Basic clipboard interaction
-vim.keymap.set({'n', 'x'}, 'cp', '"+y')
-vim.keymap.set({'n', 'x'}, 'cv', '"+p')
+-- vim.keymap.set({'n', 'x'}, 'cp', '"+y')
+-- vim.keymap.set({'n', 'x'}, 'cv', '"+p')
+-- yanked pattern is put in the system clipboard (asbjornHaland)
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- Delete text
-vim.keymap.set({'n', 'x'}, 'x', '"_x')
+-- vim.keymap.set({'n', 'x'}, 'x', '"_x')
 
 -- Commands
-vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+-- vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+vim.keymap.set("n", "<leader>w", vim.cmd.write)
 vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
 vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
+-- Set basic remaps for write/quit/back to normal from insert
+vim.keymap.set("n", "<leader>q", vim.cmd.quit)
+vim.keymap.set("i", "jj", "<Esc>")
+--vim.keymap.set("i", "<C-c>", "<Esc>")
 
+
+-- Move highlighted lines up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- take line below and append it to the end of the current line with a space
+-- keeps the cursor in the same space as well
+vim.keymap.set("n", "J", "mzJ`z")
+-- jump half page up/down (c-d/c-u)
+-- keeps the cursor in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- jump to next/prev match while keeping the cursor centered on the screen
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- WTF is vim-with-me??? keymaps for that
+--vim.keymap.set("n", "<leader>vwm", function()
+--    require("vim-with-me").StartVimWithMe()
+--end)
+--vim.keymap.set("n", "<leader>svwm", function()
+--    require("vim-with-me").StopVimWithMe()
+--end)
+
+-- preserve current paste buffer whan copying over highlighted pattern
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- deleting to void register  so deleted pattern is not preserved
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "Q", "<nop>")
+
+--vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- LSP: format buffer
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- for quickfix window navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replace word under the cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- make edited shell file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+--vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/tomlajtos/packer.lua<CR>");
+--vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,14 +1,39 @@
+-- Make line numbers default
 vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode
 vim.opt.mouse = 'a'
+
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
-vim.opt.wrap = true
-vim.opt.breakindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.incsearch = true
+vim.opt.wrap = true --false
+
+-- Set tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
+
+vim.opt.smartindent = true
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Decrease update time
+vim.opt.updatetime = 200 --50
 vim.opt.signcolumn = 'yes'
 
 vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.isfname:append("@-@")
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+-- vim.opt.colorcolumn = "90"
+-- vim.opt.guicursor = ""
 
+-- Set completeopt to have a better completion experience
+-- vim.o.completeopt = 'menuone,noselect'
