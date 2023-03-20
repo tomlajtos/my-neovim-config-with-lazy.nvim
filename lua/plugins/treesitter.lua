7ab7164c85a39818a0540697c948f1df6f23b00b
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-03-20 14:21:06 +0100

    Reorder treesitter.lua, add swap and move conf

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,22 +6,6 @@ Plugin.dependencies = {
 
 -- See :help nvim-treesitter-modules
 Plugin.opts = {
-	highlight = {
-		enable = true,
-	},
-	-- :help nvim-treesitter-textobjects-modules
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true,
-			keymaps = {
-					['af'] = '@function.outer',
-					['if'] = '@function.inner',
-					['ac'] = '@class.outer',
-					['ic'] = '@class.inner',
-			}
-		},
-	},
 	ensure_installed = {
 		'bash',
 		'c',
@@ -37,6 +21,53 @@ Plugin.opts = {
 		'typescript',
 		'tsx',
 	},
+	auto_install = true,
+	sync_install = false,
+	highlight = {
+		enable = true,
+	},
+	-- :help nvim-treesitter-textobjects-modules
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				['af'] = '@function.outer',
+				['if'] = '@function.inner',
+				['ac'] = '@class.outer',
+				['ic'] = '@class.inner',
+			}
+		},
+	},
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
 }
 
 function Plugin.config(name, opts)
