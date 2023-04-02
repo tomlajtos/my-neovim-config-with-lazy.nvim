0ea263445029fb65904060159a9e4e409cd563fc
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-04-02 13:57:22 +0200

    Add background-off toggle for 3rd colorscheme
    - refactor toggle_transparency()
    - add generic bg off toggle for 3rd colorscheme
    - change keymaps
    - add keymap for 3rd cs

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -5,106 +5,111 @@ function Get_style_index(tbl, val)
 			return i
 		end
 	end
-	return 1 --nil
+	return 0 --return 1 insted of nil in case options.style cannot be read
 end
 
+-- toggles the style of a colorscheme  based on the available styles
+-- provided in form of a style list
+-- used in place of the colorscheme's internal toggle function if exeist
 function Toggle_style(colors, style_list)
-	local config_str = colors .. ".config"
-	print('togglef conf str:', config_str)
+	local config_str
+	local style
+-- check if right colorscheme and get the current style
 	if vim.g.colors_name == colors then
-		print('togglef colors arg:', colors)
-		style = require(config_str).options.style
-		print('toggelf style in if:', style)
-		-- else
-		-- 	style = default_style
+		if colors == 'onedark' then
+			style = vim.g.onedark_config.style
+		else
+			config_str = colors .. ".config"
+			style = require(config_str).options.style
+		end
 	end
-	local style_index = Get_style_index(style_list, style) --or 1
+-- get the index of current style and set new style index
+	local style_index = Get_style_index(style_list, style)
 	local index = style_index + 1
 
 	if index > #style_list then index = 1 end
 
 	style = style_list[index]
-	print('togglef res:', style)
+-- fixes issue with background style missmatch
+	if style == 'light' then vim.o.background = 'light' else vim.o.background = 'dark' end
+
 	return style
 end
 
 -- SET colorscheme to 'onedark'
+-- toggle option same keymap that sets the colorscheme will togle a new theme every time
+-- replaces ondark's own toggle function, works with other colorschemes better thisway
 -- @param style, str: 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
-function set_onedark(style)
-	style = style or "darker"
-	-- local bg_transparency = vim.g.onedark_config.transparent
+function Set_onedark()
+	local style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }
+	local style = Toggle_style("onedark", style_list)
 
 	require('onedark').setup({
 		disable_background = true,
 		style = style,
 		term_colors = true,
-		transparent = true,
-		diagnostics = {
-			darker = true,
-			undercurl = true,
-			background = true,
-		},
-		-- toggle theme style ---
-		toggle_style_key = '<F12>', --Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-		toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
 	})
 	vim.cmd.colorscheme('onedark')
 end
 
 -- SET colorscheme to 'tokyonight'
+-- toggle option same keymap that sets the colorscheme will togle a new theme every time
 -- @param style, str: 'night', 'dusk', 'moon', 'day'
-function set_tokyo()
-	style_list = { 'night', 'moon', 'dusk', 'day' }
+function Set_tokyo()
+	local style_list = { 'day', 'night', 'moon', 'dusk' }
 	local style = Toggle_style("tokyonight", style_list)
-	print('set_tokyof style:', style)
-
-	local transparent = require("tokyonight.config").options.transparent
-
+local curr_transp_v = require("tokyonight.config").options.transparent
 	require('tokyonight').setup({
 		style = style,
 		terminal_colors = true,
-		transparent = transparent,
+		transparent = curr_transp_v,
 		styles = {
-			-- sidebars = transparent,
-			-- floats = transparent,
+			sidebars = 'transparent',
+			floats = 'transparent',
 		}
 	})
-
 	vim.cmd.colorscheme('tokyonight')
 end
 
 -- Toggle background transparency
+-- alters colorscheme config
 local toggle_transparency = function()
 	local colorscheme = vim.g.colors_name
-
+if colorscheme == "onedark" or colorscheme == "tokyonight" then
 	if colorscheme == "onedark" then
-		bg_transparency = vim.g.onedark_config.transparent
-		cs_style = vim.g.onedark_config.style
+		BG_transparency = vim.g.onedark_config.transparent
+		CS_style = vim.g.onedark_config.style
 	elseif colorscheme == "tokyonight" then
-		bg_transparency = require("tokyonight.config").options.transparent
-		cs_style = require("tokyonight.config").options.style
+		BG_transparency = require("tokyonight.config").options.transparent
+		CS_style = require("tokyonight.config").options.style
 	end
 
-	if bg_transparency then
+	if BG_transparency then
 		require(colorscheme).setup({
 			transparent = false,
-			style = cs_style,
+			style = CS_style,
 		})
 	else
 		require(colorscheme).setup({
 			transparent = true,
 		})
 	end
-
 	vim.cmd.colorscheme(colorscheme)
+else
+	vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'FoatBorder', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'FoatTitle', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'MsgArea', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'NonText', { bg = 'none' })
+end
 end
 
--- vim.keymap.set("n", "<F9>", function()Toggle_style("tokyonight", { 'night', 'moon', 'dusk', 'day' })
--- vim.keymap.set("n", "<F9>", function()Toggle_style("onedark", { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' })
--- end)
-vim.keymap.set("n", "<F10>", function() toggle_transparency() end)
-vim.keymap.set("n", "<F11>", function() set_tokyo() end)
--- <F12> sets onedark colorscheme and toggles the different styles
+vim.keymap.set("n", "<F9>", function() toggle_transparency() end)
+vim.keymap.set("n", "<F10>", function() vim.cmd.colorscheme('darkplus') end)
+vim.keymap.set("n", "<F11>", function() Set_tokyo() end)
+vim.keymap.set("n", "<F12>", function() Set_onedark() end)
 
-print('colors.lua cs:', vim.g.colors_name)
-set_onedark("darker")
+Set_onedark()
