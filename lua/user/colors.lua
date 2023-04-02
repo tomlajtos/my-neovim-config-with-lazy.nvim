7615cd2535737ff589631e93be015a0455455788
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-04-02 15:06:06 +0200

    Use map for keymaps

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -14,7 +14,7 @@ end
 function Toggle_style(colors, style_list)
 	local config_str
 	local style
--- check if right colorscheme and get the current style
+	-- check if right colorscheme and get the current style
 	if vim.g.colors_name == colors then
 		if colors == 'onedark' then
 			style = vim.g.onedark_config.style
@@ -23,14 +23,14 @@ function Toggle_style(colors, style_list)
 			style = require(config_str).options.style
 		end
 	end
--- get the index of current style and set new style index
+	-- get the index of current style and set new style index
 	local style_index = Get_style_index(style_list, style)
 	local index = style_index + 1
 
 	if index > #style_list then index = 1 end
 
 	style = style_list[index]
--- fixes issue with background style missmatch
+	-- fixes issue with background style missmatch
 	if style == 'light' then vim.o.background = 'light' else vim.o.background = 'dark' end
 
 	return style
@@ -56,9 +56,9 @@ end
 -- toggle option same keymap that sets the colorscheme will togle a new theme every time
 -- @param style, str: 'night', 'dusk', 'moon', 'day'
 function Set_tokyo()
-	local style_list = { 'day', 'night', 'moon', 'dusk' }
+	local style_list = { 'night', 'moon', 'dusk', 'day', }
 	local style = Toggle_style("tokyonight", style_list)
-local curr_transp_v = require("tokyonight.config").options.transparent
+	local curr_transp_v = require("tokyonight.config").options.transparent
 	require('tokyonight').setup({
 		style = style,
 		terminal_colors = true,
@@ -75,41 +75,45 @@ end
 -- alters colorscheme config
 local toggle_transparency = function()
 	local colorscheme = vim.g.colors_name
-if colorscheme == "onedark" or colorscheme == "tokyonight" then
-	if colorscheme == "onedark" then
-		BG_transparency = vim.g.onedark_config.transparent
-		CS_style = vim.g.onedark_config.style
-	elseif colorscheme == "tokyonight" then
-		BG_transparency = require("tokyonight.config").options.transparent
-		CS_style = require("tokyonight.config").options.style
-	end
+	if colorscheme == "onedark" or colorscheme == "tokyonight" then
+		if colorscheme == "onedark" then
+			BG_transparency = vim.g.onedark_config.transparent
+			CS_style = vim.g.onedark_config.style
+		elseif colorscheme == "tokyonight" then
+			BG_transparency = require("tokyonight.config").options.transparent
+			CS_style = require("tokyonight.config").options.style
+		end
 
-	if BG_transparency then
-		require(colorscheme).setup({
-			transparent = false,
-			style = CS_style,
-		})
+		if BG_transparency then
+			require(colorscheme).setup({
+				transparent = false,
+				style = CS_style,
+			})
+		else
+			require(colorscheme).setup({
+				transparent = true,
+			})
+		end
+		vim.cmd.colorscheme(colorscheme)
 	else
-		require(colorscheme).setup({
-			transparent = true,
-		})
+		vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+		vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+		vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+		vim.api.nvim_set_hl(0, 'FoatBorder', { bg = 'none' })
+		vim.api.nvim_set_hl(0, 'FoatTitle', { bg = 'none' })
+		vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+		vim.api.nvim_set_hl(0, 'MsgArea', { bg = 'none' })
+		vim.api.nvim_set_hl(0, 'NonText', { bg = 'none' })
 	end
-	vim.cmd.colorscheme(colorscheme)
-else
-	vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-	vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
-	vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-	vim.api.nvim_set_hl(0, 'FoatBorder', { bg = 'none' })
-	vim.api.nvim_set_hl(0, 'FoatTitle', { bg = 'none' })
-	vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
-	vim.api.nvim_set_hl(0, 'MsgArea', { bg = 'none' })
-	vim.api.nvim_set_hl(0, 'NonText', { bg = 'none' })
-end
 end
 
-vim.keymap.set("n", "<F9>", function() toggle_transparency() end)
-vim.keymap.set("n", "<F10>", function() vim.cmd.colorscheme('darkplus') end)
-vim.keymap.set("n", "<F11>", function() Set_tokyo() end)
-vim.keymap.set("n", "<F12>", function() Set_onedark() end)
+-- set keymaps with map(mode, lhs, rhs, desc, plugin*) *optional
+local map = require("user/keymaps").map
 
+map("n", "<F9>", function() toggle_transparency() end, "toggle bg transparency")
+map("n", "<F10>", function() vim.cmd.colorscheme('darkplus') end, "Set 'darkplus'", "Colorscheme")
+map("n", "<F11>", function() Set_tokyo() end, "Set 'tokyonight'", "Colorscheme")
+map("n", "<F12>", function() Set_onedark() end, "Set 'onedark'", "Colorscheme")
+
+-- set default colorscheme
 Set_onedark()
