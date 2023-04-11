e0b4c1ebec504d6852b575b0dc222cf286f15355
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-04-11 22:13:32 +0200

    Add user command 'ColorschemeInfo'

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -2,16 +2,24 @@ local group = vim.api.nvim_create_augroup('user_cmds', { clear = true })
 
 vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
 
+vim.api.nvim_create_user_command('ColorschemeInfo', function()
+	print(
+		"Colorscheme loaded:", vim.g.cs_loaded, --[[ "\n", ]]
+		"> Colorscheme:", vim.g.colors_name, --[[ "\n", ]]
+		"> Transparent bg:", vim.g.transp_cs_bg, --[[ "\n", ]]
+		"> Current style:", vim.g.curr_cs_style --[[ "\n", ]])
+end, {})
+
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight on yank',
-  group = group,
-  callback = function()
-    vim.highlight.on_yank({ higroup = 'Visual', timeout = 200 })
-  end,
+	desc = 'Highlight on yank',
+	group = group,
+	callback = function()
+		vim.highlight.on_yank({ higroup = 'Visual', timeout = 200 })
+	end,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'help', 'man' },
-  group = group,
-  command = 'nnoremap <buffer> q <cmd>quit<cr>'
+	pattern = { 'help', 'man' },
+	group = group,
+	command = 'nnoremap <buffer> q <cmd>quit<cr>'
 })
